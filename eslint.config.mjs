7a73d3c8d24422cf647de6360d4f350b4aaf91ec
Dev Talan <chaudhary.dev.talan@gmail.com>
2025-02-22 17:37:20 +0530

    ci: implemented husky and commitlint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    rules: {
+      'no-unused-vars': 'warn',
+      '@typescript-eslint/no-unused-vars': 'warn', // Fixes the error rule
+    },
+  },
 ];
 
 export default eslintConfig;
