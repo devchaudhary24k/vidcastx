f876d64f0787662b1273b86dfdfd88f45f2d725b
Dev Talan <chaudhary.dev.talan@gmail.com>
2025-12-07 12:30:43 +0530

    org plugin in client auth

--- apps/dashboard/src/lib/auth.ts
+++ apps/dashboard/src/lib/auth.ts
@@ -1,7 +1,9 @@
+import { organizationClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 
 export type BetterAuthClient = ReturnType<typeof createAuthClient>;
 
 export const auth: BetterAuthClient = createAuthClient({
   baseURL: "http://localhost:3000",
+  plugins: [organizationClient()],
 });
