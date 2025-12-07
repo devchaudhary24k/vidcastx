76dc024acdebbef89d4af2b22b73bf736ad5fe6d
Dev Talan <chaudhary.dev.talan@gmail.com>
2025-12-07 13:32:35 +0530

    updated types in auth client

--- apps/dashboard/src/lib/auth.ts
+++ apps/dashboard/src/lib/auth.ts
@@ -1,9 +1,17 @@
-import { organizationClient } from "better-auth/client/plugins";
+import { type auth as authServer } from "@server/auth/auth";
+import {
+  inferAdditionalFields,
+  inferOrgAdditionalFields,
+  organizationClient,
+} from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 
-export type BetterAuthClient = ReturnType<typeof createAuthClient>;
-
-export const auth: BetterAuthClient = createAuthClient({
+export const auth = createAuthClient({
   baseURL: "http://localhost:3000",
-  plugins: [organizationClient()],
+  plugins: [
+    organizationClient({
+      schema: inferOrgAdditionalFields<typeof authServer>(),
+    }),
+    inferAdditionalFields<typeof authServer>(),
+  ],
 });
