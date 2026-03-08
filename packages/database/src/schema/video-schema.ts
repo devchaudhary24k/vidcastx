2dec75a520cbff74ca2858d2f3cf13a1ad88a5cd
Dev Talan <chaudhary.dev.talan@gmail.com>
2026-03-08 17:20:42 +0530

    implemented worker

--- packages/database/src/schema/video-schema.ts
+++ packages/database/src/schema/video-schema.ts
@@ -47,6 +47,7 @@ export const videos = pgTable(
     aspectRatio: text("aspect_ratio"), // The aspect ratio of the video
     frameCount: integer("frame_count"), // The frame count of the video
     masterAccessUrl: text("master_access_url"), // The master access URL for the video
+    playbackUrl: text("playback_url"), // The playback URL for the video
     metadata: jsonb("metadata").default({}), // Metadata associated with the video
     createdAt: timestamp("created_at").defaultNow().notNull(), // The timestamp when the video was created
     updatedAt: timestamp("updated_at")
