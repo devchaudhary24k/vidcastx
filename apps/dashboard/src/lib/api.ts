f3649657aa8adbac932dee8f4e866d38a28b9fb0
Dev Talan <chaudhary.dev.talan@gmail.com>
2026-03-10 22:44:39 +0530

    fixed build errors

--- apps/dashboard/src/lib/api.ts
+++ apps/dashboard/src/lib/api.ts
@@ -1,5 +1,5 @@
+import type { App } from "@server/server";
 import { treaty } from "@elysiajs/eden";
-import { type App } from "@server/server";
 
 const client = treaty<App>("http://localhost:3000");
 
